b36764ad1f68ae62bcd5c9541917ec88dcbbf826
medhavenkatraj <medhav@nycstudents.net>
2025-10-16 06:53:20 -0700

    Update python.pp

--- python.pp
+++ python.pp
@@ -1,7 +1,15 @@
+
+
 item = {
 "name": "oreos",
 "price": 3.99,
 "department": "desert",
 "description": "chocolate cookie with creamy vanilla filling"
 }
-print(item)
+
+print("would you like to buy oreos, apple juice or vanilla icecream? ")
+if input:
+    "oreos"
+    print(item[0])
+
+print(item["name"])
