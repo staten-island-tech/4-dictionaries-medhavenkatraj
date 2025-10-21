f2410ddc408a903c5d9d3e9008d7154cef12def6
medhavenkatraj <medhav@nycstudents.net>
2025-10-21 07:06:02 -0700

    10/21/25

--- python.pp
+++ python.pp
@@ -1,15 +1,31 @@
+item = {
+    "name": "oreos",
+    "price": 3.99,
+    "department": "desert",
+    "description": "chocolate cookie with creamy vanilla filling"
+}
 
 
 item = {
-"name": "oreos",
-"price": 3.99,
-"department": "desert",
-"description": "chocolate cookie with creamy vanilla filling"
+    "name": "apple juice",
+    "price": 2.99,
+    "department": "drinks",
+    "description": "sweet, tart, and refreshing "
 }
 
-print("would you like to buy oreos, apple juice or vanilla icecream? ")
-if input:
-    "oreos"
-    print(item[0])
+item = {
+    "name": "vanilla icecream",
+    "price": 3.99,
+    "department": "icecream",
+    "description": "cold, sweet, and refreshing treat "
+}
 
-print(item["name"])
+
+print(input("would you like to buy oreos, apple juice or vanilla icecream? "))
+if input == ["oreos", "apple juice", "vanilla icecream"]:
+    print(item["name"])
+print(input("would you like to continue shopping? "))
+if input == ("yes"):
+        print(input("would you like to buy oreos, apple juice or vanilla icecream? "))
+elif input == ("no"):
+        print(input("thank you for shopping here!", item))
