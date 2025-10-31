baeece0b7ed3e7e9b415147312cdf7af6444a58c
medhavenkatraj <medhav@nycstudents.net>
2025-10-31 07:14:39 -0700

    Update python.pp

--- python.pp
+++ python.pp
@@ -8,7 +8,7 @@ item = {
 
 item = {
     "name": "apple juice",
-    "price": 2.99,
+    "price": 3.99,
     "department": "drinks",
     "description": "sweet, tart, and refreshing "
 }
@@ -20,12 +20,32 @@ item = {
     "description": "cold, sweet, and refreshing treat "
 }
 
-
+""" cart = [] 
 print(input("would you like to buy oreos, apple juice or vanilla icecream? "))
-if input == ["oreos", "apple juice", "vanilla icecream"]:
-    print(item["name"])
+if item == ["oreos", "apple juice", "vanilla icecream"]:
+    cart.append(item) 
 print(input("would you like to continue shopping? "))
-if input == ("yes"):
-    print("would you like to buy oreos, apple juice or vanilla icecream? reply with yes or no:  ")
+print("would you like to buy oreos, apple juice or vanilla icecream? reply with yes or no:  ")
 elif input == ("no"):
-    print(("thank you for shopping here!", item))
+    print(("thank you for shopping here!", item)
+elif input:
+    print("thank you for shopping here!", cart)
+print("would you like to buy oreos, apple juice or vanilla icecream? reply with yes or no:  ")
+print(item["price"]) """
+""" print(input("would ya like to continue shopping? "))
+if input == ("yes"):
+    print("pick another item! ") """
+
+
+cart = []
+print(input("welcome! would you like to buy oreos, apple juice, or vanilla icecream? "))
+if item == ["oreos", "apple juice", "vanilla icecrean" ]:
+    cart.append(item)
+print("thanks for shopping here! ")
+print(input("do u wish to continue, reply with yes or no: "))
+if input == "no":
+    print("your total price is:", item["price"])
+if input == "yes":
+    print("what woould you like: oreos, apple juice, or vanilla icecream?" )
+for i in cart:
+    print(i)
