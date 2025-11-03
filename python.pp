7985739594a996f98a3432329e346cd4c11ff55a
medhavenkatraj <medhav@nycstudents.net>
2025-11-03 07:16:31 -0800

    11/3/25

--- python.pp
+++ python.pp
@@ -1,51 +1,35 @@
-item = {
+best_buy_items = {
+{
     "name": "oreos",
     "price": 3.99,
     "department": "desert",
     "description": "chocolate cookie with creamy vanilla filling"
-}
+},
 
 
-item = {
+{
     "name": "apple juice",
     "price": 3.99,
     "department": "drinks",
     "description": "sweet, tart, and refreshing "
-}
+},
 
-item = {
+{
     "name": "vanilla icecream",
     "price": 3.99,
     "department": "icecream",
     "description": "cold, sweet, and refreshing treat "
+},
 }
-
-""" cart = [] 
-print(input("would you like to buy oreos, apple juice or vanilla icecream? "))
-if item == ["oreos", "apple juice", "vanilla icecream"]:
-    cart.append(item) 
-print(input("would you like to continue shopping? "))
-print("would you like to buy oreos, apple juice or vanilla icecream? reply with yes or no:  ")
-elif input == ("no"):
-    print(("thank you for shopping here!", item)
-elif input:
-    print("thank you for shopping here!", cart)
-print("would you like to buy oreos, apple juice or vanilla icecream? reply with yes or no:  ")
-print(item["price"]) """
-""" print(input("would ya like to continue shopping? "))
-if input == ("yes"):
-    print("pick another item! ") """
-
-
 cart = []
 print(input("welcome! would you like to buy oreos, apple juice, or vanilla icecream? "))
-if item == ["oreos", "apple juice", "vanilla icecrean" ]:
-    cart.append(item)
+if input == ["oreos", "apple juice", "vanilla icecrean" ]:
+    cart.append(best_buy_items)
 print("thanks for shopping here! ")
 print(input("do u wish to continue, reply with yes or no: "))
 if input == "no":
-    print("your total price is:", item["price"])
-if input == "yes":
+    print("your total price is:", best_buy_items["price"])
+elif input == "yes":
     print("what woould you like: oreos, apple juice, or vanilla icecream?" )
-for i in cart:
-    print(i)
+for index, best_buy_items in enumerate(best_buy_items):
+    print(index, ":", best_buy_items["name"])
